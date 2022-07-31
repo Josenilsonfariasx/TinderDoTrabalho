5ecd6c80eac8759a225176e390653a953571b2c7
Josenilsonfariasx <fariaslwork@gmail.com>
2022-07-31 11:06:54 -0300

    Atualizando templates administracao e validando login admin

--- administracao/views.py
+++ administracao/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from hashlib import sha256
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 # Create your views here.
 
 def login(request):
-    return render(request, 'base.html')
+    return render(request, 'login.html')
+
+def valida_login(request):
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+
+    senha = sha256 (senha.encode()).hexdigest()
+
+    if email == 'fariasx@gmail.com' and senha == 'admin':
+        return redirect('/auth/home') 
+    else:
+        return redirect('/auth/login/?status=1')
+
+def home(request):
+    return render(request, 'home.tml')
