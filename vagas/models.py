3fe4050cfc9c0bc87db7ee87fe9c28c3f877c243
Josenilsonfariasx <fariaslwork@gmail.com>
2022-07-31 09:10:34 -0300

    Modificacores na /vagas

--- vagas/models.py
+++ vagas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Vagas(models.Model):
+    nome = models.CharField(max_length = 100)
+    descricao = models.TextField(max_length=2000)
+    faixa_salarial = models.TextField(max_length=20)
+    requisitos = models.TextField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Vaga'
+    def __str__(self):
+        return self.nome
+
